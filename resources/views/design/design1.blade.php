9700fa347ffee204787fd7e3381bba49555e06b3
rytis klimavicius <rytiskli@gmail.com>
2022-05-23 13:15:03 +0300

    a lot more on design side

--- resources/views/design/design1.blade.php
+++ resources/views/design/design1.blade.php
@@ -4,31 +4,41 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9">
-                <div class="mt-3 mb-0 text-muted mx-3">
-                    <a href="#" class="filter-option text-muted">All</a>
-                    <a href="#" class="filter-option text-muted">Laravel</a>
-                    <a href="#" class="filter-option text-muted">Livewire</a>
-                    <a href="#" class="filter-option text-muted">Vue</a>
-                    <hr class="mt-1">
+                <div class="mb-0 mx-3">
+                    <img src="assets/images/bg-img.png" class="card-img-top" alt="...">
+                </div>
+                <div class="mb-0 row justify-content-center">
+                    <div class="col-12 text-center avatar-section">
+                        <img src="assets/images/img-avatar.png" class="card-img-top">
+                    </div>
+                    <div class="col-12 text-center text-custom-title">
+                        Average Developer
+                    </div>
+                </div>
+                <div class="mb-0 mt-2 row justify-content-center">
+                    <div class="col-6 text-center text-custom-small">
+                        Hey guys here you can find all the products and items I own and use myself daily. If L l you are
+                        interested in any of them,support me and grab them while they are available!!
+                    </div>
+                </div>
+                <div class="mb-0 mt-3 mx-3 header-custom-medium">
+                    <span>Blogs</span>
                 </div>
                 <div class="row mt-4">
-                    @for($i =0;$i<3;$i++)
+                    @for($i =0;$i<5;$i++)
                         <div class="col-4 d-flex justify-content-center m-0">
-                            <div class="card" style="width: 18rem;">
-                                <img src="assets/images/img1.png" class="card-img-top" alt="...">
-                                <div class="m-2">
-                                    <span class="badge rounded-pill bg-primary">Primary</span>
-                                    <span class="badge rounded-pill bg-secondary">Secondary</span>
-                                    <span class="badge rounded-pill bg-success">Success</span>
-                                    <span class="badge rounded-pill bg-danger">Danger</span>
-                                    <span class="badge rounded-pill bg-warning text-dark">Warning</span>
+                            <div class="card mx-3 mb-5">
+                                <div class="card-title">Laravel 0Auth2 authentication for big boys. + some vue</div>
+                                <img src="assets/images/img.png" class="card-img-top">
+                                <div class="my-2">
+                                    <span class="custom-tag tag-light-green">Laravel</span>
+                                    <span class="custom-tag tag-dark-blue">Vue</span>
+                                    <span class="custom-tag tag-red">Vuex</span>
                                 </div>
-                                <div class="card-body">
-                                    <h5 class="card-title fw-bold">Card title</h5>
-                                    <p class="card-text text-muted">Some quick example text to build on the card title
-                                        and make up the
-                                        bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-custom-primary">Read full article</a>
+                                <div class="text-custom-small">This topix cover some very simple guide opnm how to
+                                    asd lasd authentication and some otherlea. </div>
+                                <div class="mt-2">
+                                    <a href="#" class="btn btn-custom-neutral"><span class="btn-custom-text">Read article</span></a>
                                 </div>
                             </div>
                         </div>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -91,7 +91,6 @@
 {{--                </ul>--}}
             </div>
     </nav>
-    <br><br>
 {{--    <div class="title-area">--}}
 {{--        <div class="row">--}}
 {{--            <div class="col-4 offset-4 text-center mb-3">--}}
@@ -106,6 +105,24 @@
     <main class="py-4">
         @yield('content')
     </main>
+
+</div>
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+            <span class="text-muted">© 2022 Rytis Klimavicius</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <span>Icons...</span>
+{{--            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>--}}
+{{--            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>--}}
+{{--            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>--}}
+        </ul>
+    </footer>
 </div>
 </body>
 </html>
