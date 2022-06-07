096703b2900e770690e9955ccd760cb22c591e02
rytis klimavicius <rytiskli@gmail.com>
2022-06-08 00:34:35 +0300

    added very basic create form

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -4,21 +4,45 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9">
-                <div class="mb-0 row justify-content-center">
-                    <div class="col-12 text-center text-custom-title">
-                        Create screen
+                <div class="row mt-1">
+                    <div class="col-md-12">
+                        <div class="">
+                            <label class="form-label" for="email">Title</label>
+                            <input type="email" class="form-control" id="email" placeholder="Your email">
+                        </div>
+                    </div>
+                    <div class="col-md-12 my-3">
+                        <div class="">
+                            <label class="form-label" for="email">Tip</label>
+                            <input type="email" class="form-control" id="email" placeholder="Your email">
+                        </div>
+                    </div>
+                    <div class="col-md-12 my-3">
+                        <div class="">
+                            <label class="form-label" for="email">Summary</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                    </div>
+                    <div class="col-md-12 my-3">
+                        <div class="">
+                            <label class="form-label" for="cardholder">Description</label>
+                            <textarea type="text" class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12 my-3">
+                        <label class="form-label" for="input_file">Choose file</label>
+                        <input type="file" class="form-control" id="input_file" placeholder="Your email">
                     </div>
                 </div>
-                <div class="mb-0 mt-2 row justify-content-center">
-                    <div class="col-12 col-md-6 text-center text-custom-small">
-                        Hey guys here you can find all the products and items I own and use myself daily. If L l you are
-                        interested in any of them,support me and grab them while they are available!!
+                <div class="">
+                    <button type="submit" class="btn btn-custom-neutral">Save</button>
+                    <button type="submit" class="btn btn-custom-primary">Post blog</button>
+                    <div
+                        class="d-flex align-items-center justify-content-center text-center mt-5 mb-2 text-muted text-sm">
+                        <i class="bi bi-lock me-2"></i>
+                        </span>
                     </div>
-                </div>
-                <div class="mb-0 mt-3 mx-3 header-custom-medium">
-                    <span>Blogs</span>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
