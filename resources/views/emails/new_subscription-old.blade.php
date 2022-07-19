ed603b7c8e436b75fa0225a1e8ad6f121cbcdb24
rytis klimavicius <rytiskli@gmail.com>
2022-07-19 12:55:01 +0300

    html changes

---
+++ resources/views/emails/new_subscription-old.blade.php
@@ -0,0 +1,7 @@
+<h3>Thank you for subscribing {{ $email }}</h3>
+
+<a href="{{ $unsubscribeUrl }}">Click here to unsubscribe</a>
+
+<p> If you are having issues unsubscribing, you can do it by manually pasting the link into the
+    browser <br> {{ $unsubscribeUrl }} </p>
+
