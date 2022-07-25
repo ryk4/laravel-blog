c960819dcea7f150db276b163fe5bf36df074697
rytis klimavicius <rytiskli@gmail.com>
2022-07-25 23:52:07 +0300

    user seeder update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'id' => 1,
             'name' => 'Rytis Klimavicius',
-            'email' => 'rytiskli@admin.com',
+            'email' => 'rytiskli@gmail.com',
             'role' => User::ROLE_ADMIN,
             'password' => Hash::make('Sudas123'),
             'created_at' => Carbon::now(),
