04d33b564d11daceefb30c1a1366a8e5165b25e4
rytis klimavicius <rytiskli@gmail.com>
2022-08-13 11:54:12 +0300

    more tests added

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -7,11 +7,15 @@ use Tests\TestCase;
 
 class BlogTest extends TestCase
 {
+    private Blog $blog;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         self::authenticateAsAdmin();
+
+        $this->blog = Blog::factory()->create();
     }
 
     public function test_homepage_redirects_properly_to_blogs_index()
@@ -30,8 +34,18 @@ class BlogTest extends TestCase
 
     public function test_blog_create_only_admin_can_access_page()
     {
-    }
+        self::authenticateAsGuest();
+        $response = $this->get(route('admin.blogs.create'));
+        $response->assertStatus(302);
 
+        self::authenticateAsNormalUser();
+        $response = $this->get(route('admin.blogs.create'));
+        $response->assertStatus(403);
+
+        self::authenticateAsAdmin();
+        $response = $this->get(route('admin.blogs.create'));
+        $response->assertStatus(200);
+    }
 
     public function test_blog_create()
     {
@@ -52,14 +66,23 @@ class BlogTest extends TestCase
 
         $blog = Blog::orderByDesc('created_at')->first();
 
-        self::assertEquals($data['title'], $blog->title);
-        self::assertEquals('basic summary', $blog->summary);
+        self::assertDatabaseHas('blogs', ['title' => $data['title']]);
+        self::assertDatabaseHas('blogs', ['summary' => $data['summary']]);
 
         $response->assertRedirect(route('blogs.index'));
     }
 
     public function test_blog_create_redirects_back_if_validation_fails()
     {
+        $data = [
+            'title' => 'test title',
+            'guide' => 'guide test',
+            'repository_url' => 'wrong url',
+        ];
+
+        $response = $this->post(route('admin.blogs.store', $data));
+        $response->assertStatus(302);
+        $response->assertInvalid(['summary', 'repository_url']);
     }
 
     public function test_blog_create_attaches_image_correctly()
@@ -70,12 +93,29 @@ class BlogTest extends TestCase
     {
     }
 
-    public function test_blog_update()
+    public function test_blog_update_updates_data_correctly()
     {
+        $data = [
+            'title' => 'new title',
+            'guide' => 'new guide',
+            'summary' => $this->blog->summary,
+        ];
+
+        $response = $this->put(route('admin.blogs.update', $this->blog), $data);
+
+        $response->assertStatus(302);
+
+        self::assertDatabaseHas('blogs', ['title' => $data['title']]);
+        self::assertDatabaseHas('blogs', ['guide' => $data['guide']]);
     }
 
     public function test_blog_delete()
     {
+        $response = $this->delete(route('admin.blogs.destroy', $this->blog));
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('admin.blogs.index'));
+        self::assertDatabaseMissing('blogs', ['title' => $this->blog->title]);
     }
 
     public function test_blog_guest_users_cannot_manage_blogs()
@@ -89,5 +129,4 @@ class BlogTest extends TestCase
     public function test_blog_show_redirects_to_correct_page()
     {
     }
-
 }
